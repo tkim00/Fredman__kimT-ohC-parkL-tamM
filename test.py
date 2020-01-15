a2d74be2e37d7399186449215b133d60001a6cda
cohyunoh <coh00@stuy.edu>
2020-01-15 01:23:54 -0500

    Added navbar with success and added the actual game screen and text input

--- test.py
+++ test.py
@@ -22,9 +22,13 @@ def main():
 def game():
     return render_template('gamescreen.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route("/logout")
 def logout():
-    return render_template('login.html')
+    return redirect(url_for('login'))
 if __name__ == "__main__":
    app.debug = True
    app.run()
